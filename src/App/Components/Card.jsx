f1e80656295041c56f7d94b82c08d0637776fe18
Todd Turnbull <shingaling@gmail.com>
2021-08-29 12:05:43 -0400

    Add react-markdown for formatting Card Description

--- src/App/Components/Card.jsx
+++ src/App/Components/Card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ReactMarkdown from "react-markdown"
 
 const NodeIconSize = {
   width: 48,
@@ -42,7 +43,6 @@ const Description = styled.div`
   line-height: 20px;
   letter-spacing: 0.01em;
   color: #000000;
-  white-space: pre-wrap;
   margin-bottom: 13.4px;
 `;
 
@@ -93,7 +93,7 @@ export default class Card extends React.Component {
           }}
         >
           <Title>{this.props.title}</Title>
-          <Description>{this.props.description}</Description>
+          <Description><ReactMarkdown source={this.props.description} /></Description>
           <div>{this.IconsExist(this.props.flowchart_icons)}</div>
         </div>
         {onClick && (
